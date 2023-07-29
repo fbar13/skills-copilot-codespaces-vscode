a45768ab77d82f5cfd90736105d3e958669cba73
fbar13 <39195355+fbar13@users.noreply.github.com>
2023-07-29 15:03:17 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,46 +1,19 @@
 // Create web server
 
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const Comment = require('./models/comment');
+const cors = require('cors');
 
-// Connect to db
-mongoose.connect('mongodb://localhost:27017/commentDB', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.log('Could not connect to MongoDB...'));
+const app = express();
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
-// Routes
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const comments = require('./routes/api/comments');
 
-app.get('/comments', (req, res) => {
-    Comment.find().then((comments) => {
-        res.send(comments);
-    });
-});
+app.use('/api/comments', comments);
 
-app.post('/comments', (req, res) => {
-    const comment = new Comment({
-        name: req.body.name,
-        comment: req.body.comment
-    });
-    comment.save().then(() => {
-        res.send(comment);
-    });
-});
+const port = process.env.PORT || 5000;
 
-app.delete('/comments/:id', (req, res) => {
-    Comment.findByIdAndDelete(req.params.id).then((comment) => {
-        res.send(comment);
-    });
-});
-
-// Listen
-app.listen(3000);
+app.listen(port, () => console.log(`Server started on port ${port}`));
